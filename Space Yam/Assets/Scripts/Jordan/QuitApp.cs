01bb1a9f8d21ff339cecdda804ed7a92d157d265
lKryptedl <102044843+lKryptedl@users.noreply.github.com>
2023-01-23 12:04:36 +0000

    Main Menu Functionality

---
+++ Space Yam/Assets/Scripts/Jordan/QuitApp.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class QuitApp : MonoBehaviour
+{
+    public void QuitDaGame()
+    {
+        print("Quitting");
+        Application.Quit();
+    }
+}
