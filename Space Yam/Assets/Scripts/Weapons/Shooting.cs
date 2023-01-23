1245c2ea44bdc23aa8c954087910674f8cff8c12
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-01-23 11:20:27 +0000

    Added new input system

---
+++ Space Yam/Assets/Scripts/Weapons/Shooting.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        var mousePos = Input.mousePosition;
+
+       // transform
+        
+    }
+}
