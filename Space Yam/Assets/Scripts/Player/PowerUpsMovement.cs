4712941f2cc0631491c8cf3cf2699ad35d87eeb8
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-01-24 21:12:38 +0000

    Added basic powerup movement system

---
+++ Space Yam/Assets/Scripts/Player/PowerUpsMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PowerUpsMovement : MonoBehaviour
+{
+    [Header("Smooth Movement")]
+    public Vector3[] nodes;
+    public GameObject powerup;
+    public float moveSpeed;
+    public float timer;
+    public Vector3 currentNodePosition;
+    public int currentNodeID;
+
+    private Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = powerup.transform.position;
+        timer = 0;
+        currentNodePosition = nodes[currentNodeID];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime * moveSpeed;
+        if (powerup.transform.position != currentNodePosition)
+        {
+            powerup.transform.position = Vector3.Lerp(startPosition, currentNodePosition, timer);
+        }
+        else if (currentNodeID < nodes.Length - 1)
+        {
+            startPosition = currentNodePosition;
+            currentNodeID++;
+            timer = 0;
+            currentNodePosition = nodes[currentNodeID];
+        }
+    }
+}
