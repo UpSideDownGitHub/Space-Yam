313fe7c41f3a0aacd91f2ccc4b77ee363019c177
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2023-01-24 18:58:47 +0000

    Improved Enemys
    improved there spawning as well as adding some new movement options for them, i also made them not shoot untill first node reached then they shoot at random intervals to make more fair

--- Space Yam/Assets/Scripts/Enemys/Enemy.cs
+++ Space Yam/Assets/Scripts/Enemys/Enemy.cs
@@ -14,6 +14,15 @@ public class Enemy : MonoBehaviour
     [Header("Path")]
     public float minDistance;
     public Vector3[] chosenPath;
+    public Vector3[] Path1;
+    public Vector3[] Path2;
+    public Vector3[] Path3;
+    public Vector3[] Path4;
+    public Vector3[] Path5;
+    public Vector3[] Path6;
+    public Vector3[] Path7;
+    public Vector3[] Path8;
+    public List<Vector3[]> paths = new List<Vector3[]>();
     public int currentPoint;
 
     [Header("================================")]
@@ -25,30 +34,60 @@ public class Enemy : MonoBehaviour
     public float attackRate;
     private float _timeSinceLastAttack;
 
+    [Header("Better Shooting")]
+    public float speed;
+    public float preNodeSpeed;
+    public float minAttackDelay;
+    public float maxAttackDelay;
+    private bool _firstTime;
+    private bool _reachedFirstNode;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        paths.Add(Path1);
+        paths.Add(Path2);
+        paths.Add(Path3);
+        paths.Add(Path4);
+        paths.Add(Path5);
+        paths.Add(Path6);
+        paths.Add(Path7);
+        paths.Add(Path8);
+
+        chosenPath = paths[Random.Range(0, paths.Count)];
+
         player = GameObject.FindGameObjectWithTag("Player");
         agent.SetDestination(new Vector3(chosenPath[currentPoint].x, chosenPath[currentPoint].y, transform.position.z));
-        _timeSinceLastAttack = Time.time;
+        _firstTime = true;
+        agent.speed = preNodeSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
         // attacking
-        if (Time.time > attackRate + _timeSinceLastAttack)
+        if (Time.time > attackRate + _timeSinceLastAttack && _reachedFirstNode)
         {
-            _timeSinceLastAttack = Time.time;
+            _timeSinceLastAttack = Time.time + Random.Range(minAttackDelay, maxAttackDelay);
             firePoint.transform.LookAt(player.transform);
             GameObject tempBullet = Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
             tempBullet.GetComponent<Rigidbody>().AddForce(tempBullet.transform.forward * force);
         }
 
+        agent.SetDestination(new Vector3(chosenPath[currentPoint].x, chosenPath[currentPoint].y, transform.position.z));
         // movement
         if (Vector2.Distance(transform.position, chosenPath[currentPoint]) < minDistance)
         {
+            if (currentPoint == 0 && _firstTime)
+            {
+                _firstTime = false;
+                _reachedFirstNode = true;
+                agent.speed = speed;
+
+                _timeSinceLastAttack = Time.time + Random.Range(minAttackDelay, maxAttackDelay);
+            }
+
             // set the destination to the new point
             currentPoint++;
             if (currentPoint == chosenPath.Length)
